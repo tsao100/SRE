4702f8501ee5c1ca7f139cc7c5c45228a81ce49d
曹政忠 (Jack Tsao) <tsao100@ms17.hinet.net>
2026-02-19 20:54:04 +0800

    Update AME2_1.c cracked.

--- AME2_1.c
+++ AME2_1.c
@@ -1,105 +1,61 @@
+/* keygen.c */
+/* MSC 6.0 16-bit compatible */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-static unsigned long calc_auth_debug(long A, long B, int flag)
+unsigned long generate_code(unsigned long serial)
 {
-    unsigned long E36C, E370, E374, E378;
-    unsigned long var_4, var_8, var_C, var_10, var_14;
-    unsigned long edi, eax, edx, temp1, temp2;
+    unsigned long A, B, C;
+    unsigned long edi;
 
-    E36C = (unsigned long)(A / 10000L);
-    E370 = (unsigned long)(A % 10000L);
-    E374 = (E36C % 100UL) + (B % 100UL) * 100UL;
-    E378 = (E36C / 100UL) + (B / 100UL) * 100UL;
+    /* split serial */
+    A = serial / 10000UL;
+    B = serial % 10000UL;
+    C = serial % 4096UL;
 
-    printf("INPUT: A=%ld, B=%ld, flag=%d\n", A, B, flag);
-    printf("E36C=%lu, E370=%lu, E374=%lu, E378=%lu\n\n", E36C, E370, E374, E378);
+    /* algorithm */
+    edi = B;
 
-    /* Step 1 */
-    edx = E370 % 256UL;
-    eax = E370 / 256UL;
-    var_C = edx;
-    edi = (eax ^ 0x41UL) + 2UL;
-    var_4 = edi;
-    printf("Step1: var_4=0x%lX\n", var_4);
+    edi += 0x6AA602B0UL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 2 */
-    var_8 = (var_4 << 23) + (var_4 << 15);
-    printf("Step2: var_8=0x%lX\n", var_8);
+    edi *= 0x075BCD15UL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 3 */
-    eax = (E374 / 256UL) ^ 0x4DUL;
-    eax += var_8 + 1UL;
-    edi = var_4 + eax;
-    var_10 = edi;
-    printf("Step3: var_10=0x%lX\n", var_10);
+    edi += 0x0000ACADUL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 4 - 详细输出 */
-    temp1 = var_10 >> 8;
-    temp2 = temp1 >> 8;
-    printf("Step4: var_10>>16 = 0x%lX\n", temp2);
-    
-    edi = temp2 + var_10;
-    printf("Step4: edi = temp2+var_10 = 0x%lX + 0x%lX = 0x%lX\n", 
-           temp2, var_10, edi);
-    
-    eax = var_10 + edi;
-    printf("Step4: eax = var_10+edi = 0x%lX + 0x%lX = 0x%lX\n", 
-           var_10, edi, eax);
-    
-    edi = eax;
-    printf("Step4: final edi = 0x%lX\n\n", edi);
+    edi *= 0x4DUL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 5 */
-    eax = (edi & 0xFFFF0000UL) | ((edi & 0xFFFFUL) ^ 0xACADUL);
-    edi = (var_C ^ 0x32UL) + eax;
-    printf("Step5: edi=0x%lX\n\n", edi);
+    edi *= 0xB1UL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 7 */
-    if (flag) {
-        var_14 = edi;
-        edi <<= 10;
-        edx = E378 % 256UL;
-        eax = E378 / 256UL;
-        eax ^= edx;
-        eax = (eax & 0xFFFFFF00UL) | ((eax & 0xFFUL) ^ 0xB1UL);
-        eax += edi;
-        edi = var_14 + eax;
-        printf("Step7: edi=0x%lX\n\n", edi);
-    }
+    edi += C;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 8 */
-    var_4 = edi;
-    if (edi & 0x80000000UL)
-        edi = (edi >> 1) | 0x80000000UL;
-    else
-        edi >>= 1;
-    edi += var_4;
-    printf("Step8: edi=0x%lX\n\n", edi);
+    edi *= 0xD2UL;
+    edi &= 0xFFFFFFFFUL;
 
-    /* Step 9 */
-    eax = E374 & 0xFFUL;
-    if ((long)E374 < 0L && eax != 0UL)
-        eax -= 0x100UL;
-    eax = (eax & 0xFFFFFF00UL) | ((eax & 0xFFUL) ^ 0xD2UL);
-    eax += edi;
-    printf("Step9: result=0x%lX\n\n", eax);
-
-    return eax;
+    return edi;
 }
 
 int main(void)
 {
-    unsigned long r1, r2, r3;
-    
-    r1 = calc_auth_debug(10524473L, 110L, 1);
-    printf("Result: 0x%08lX (expect 0x631CBEB9)\n\n", r1);
-    
-    r2 = calc_auth_debug(10304111L, 110L, 1);
-    printf("Result: 0x%08lX (expect 0x6AA602B0)\n\n", r2);
-    
-    r3 = calc_auth_debug(10058907L, 117L, 1);
-    printf("Result: 0x%08lX (expect 0xF258421D)\n\n", r3);
-    
+    char input[32];
+    unsigned long serial;
+    unsigned long code;
+
+    printf("Enter serial (format XXX-YYYYYYYY): ");
+    scanf("%31s", input);
+
+    /* 取 '-' 後面的數字 */
+    serial = strtoul(strchr(input, '-') + 1, NULL, 10);
+
+    code = generate_code(serial);
+
+    printf("Authorization Code = %08lX\n", code);
+
     return 0;
 }
